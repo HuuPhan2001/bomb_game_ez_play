99e7f95074376b767ccfec4599519252b2f644f6
leducanh <leducanh11072000@gmail.com>
2022-10-12 00:30:55 +0700

    commit destruction bomb

--- Assets/Scripts/BombController.cs
+++ Assets/Scripts/BombController.cs
@@ -1,7 +1,6 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class BombController : MonoBehaviour
 {
@@ -21,6 +20,10 @@ public class BombController : MonoBehaviour
     // bomb size
     public int explosionRadius = 1;
 
+    [Header("Destructible")]
+    public Tilemap destructibleTiles;
+    public Destructible destructiblePrefab;
+
     private void OnEnable()
     {
         bombsRemaining = bombAmount;
@@ -32,54 +35,55 @@ public class BombController : MonoBehaviour
             StartCoroutine(PlaceBomb());
         }
     }
-        private IEnumerator PlaceBomb()
+    private IEnumerator PlaceBomb()
+    {
+        Vector2 position = transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+        bombsRemaining--;
+
+        yield return new WaitForSeconds(bombFuseTime);
+
+        position = bomb.transform.position;
+        position.x = Mathf.Round(position.x);
+        position.y = Mathf.Round(position.y);
+
+        Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
+        explosion.SetActiveRenderer(explosion.start);
+        explosion.DestroyAfter(explosionDuration);
+        Destroy(explosion.gameObject, explosionDuration);
+
+        Explose(position, Vector2.up, explosionRadius);
+        Explose(position, Vector2.down, explosionRadius);
+        Explose(position, Vector2.left, explosionRadius);
+        Explose(position, Vector2.right, explosionRadius);
+
+        Destroy(bomb);
+        bombsRemaining++;
+
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Bomb"))
         {
-            Vector2 position = transform.position;
-            position.x = Mathf.Round(position.x);
-            position.y = Mathf.Round(position.y);
-
-            GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
-            bombsRemaining--;
-
-            yield return new WaitForSeconds(bombFuseTime);
-
-            position = bomb.transform.position;
-            position.x = Mathf.Round(position.x);
-            position.y = Mathf.Round(position.y);
-
-            Explosion explosion = Instantiate(explosionPrefab, position, Quaternion.identity);
-            explosion.SetActiveRenderer(explosion.start);
-            explosion.DestroyAfter(explosionDuration);
-            Destroy(explosion.gameObject, explosionDuration);
-
-            Explose(position, Vector2.up, explosionRadius);
-            Explose(position, Vector2.down, explosionRadius);
-            Explose(position, Vector2.left, explosionRadius);
-            Explose(position, Vector2.right, explosionRadius);
-
-            Destroy(bomb);
-            bombsRemaining++;
-
+            collision.isTrigger = false;
         }
-    //private void OnTriggerExit2D(Collider2D collision)
-    //{
-    //    if(collision.gameObject.layer == LayerMask.NameToLayer("Bomb"))
-    //    {
-    //        collision.isTrigger = false;
-    //    }
-    //}
+    }
 
     private void Explose(Vector2 position, Vector2 direction, int length)
     {
-        if(length <= 0)
+        if (length <= 0)
         {
             return;
         }
 
         position += direction;
 
-        if(Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
+        if (Physics2D.OverlapBox(position, Vector2.one / 2f, 0f, explosionLayerMask))
         {
+            ClearDestructible(position);
             return;
         }
 
@@ -90,5 +94,17 @@ public class BombController : MonoBehaviour
         //Destroy(explosion.gameObject, explosionDuration);
 
         Explose(position, direction, length - 1);
+    }
+    private void ClearDestructible(Vector2 position)
+    {
+        Vector3Int cell = destructibleTiles.WorldToCell(position);
+        TileBase tile = destructibleTiles.GetTile(cell);
+
+        if (tile != null)
+        {
+            Instantiate(destructiblePrefab, position, Quaternion.identity);
+            destructibleTiles.SetTile(cell, null);
+
+        }
     }
 }
