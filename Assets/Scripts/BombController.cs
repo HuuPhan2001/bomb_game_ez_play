5d906654cf6392f5ac628bdeb90a50f7b005e45c
leducanh <leducanh11072000@gmail.com>
2022-10-11 20:13:58 +0700

    finish bombController

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombController : MonoBehaviour
+{
+    public GameObject bombPrefab;
+    public KeyCode inputKey = KeyCode.Space;
+    public float bombFuseTime = 3f;
+    public int bombAmount = 1;
+    private int bombsRemaining;
+    private void OnEnable()
+    {
+        bombsRemaining = bombAmount;
+    }
+    private void Update()
+    {
+        if (bombsRemaining > 0 && Input.GetKeyDown(inputKey))
+        {
+            StartCoroutine(PlaceBomb());
+        }
+    }
+        private IEnumerator PlaceBomb()
+        {
+            Vector2 position = transform.position;
+            position.x = Mathf.Round(position.x);
+            position.y = Mathf.Round(position.y);
+
+            GameObject bomb = Instantiate(bombPrefab, position, Quaternion.identity);
+            bombsRemaining--;
+
+            yield return new WaitForSeconds(bombFuseTime);
+            Destroy(bomb);
+            bombsRemaining++;
+        }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Bomb"))
+        {
+            collision.isTrigger = false;
+        }
+    }
+}
--- README.md
+++
@@ -1,2 +0,0 @@
-# bomb_game_ez_play
-ae làm project chỉ push file trong các folder Assest, Packages, ProjectSettings, Sprites
